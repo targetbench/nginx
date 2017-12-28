c9b6e794019e679c001924835b3e6f9211cc7376
hansy <hansy0419@thundersoft.com>
2017-12-28 15:13:34 +0800

    update code

--- handlers/nginx_parser.py
+++ handlers/nginx_parser.py
@@ -2,6 +2,8 @@
 import re
 import string
 import operator
+import json
+from caliper.server.run import parser_log
 
 #finished in 2 sec, 697 millisec and 187 microsec, 9268 req/s, 7693 kbyte/s
 #requests: 25000 total, 25000 started, 25000 done, 25000 succeeded, 0 failed, 0 errored
@@ -41,13 +43,34 @@ def nginx_parser(content, outfp):
 
     return dic
 
+def nginx(filePath, outfp):
+    cases = parser_log.parseData(filePath)
+    result = []
+    for case in cases:
+        caseDict = {}
+        caseDict[parser_log.BOTTOM] = parser_log.getBottom(case)
+        titleGroup = re.search('\[([\ \S]+)\]', case)
+        if titleGroup != None:
+            caseDict[parser_log.TOP] = titleGroup.group(0)
+            caseDict[parser_log.BOTTOM] = parser_log.getBottom(case)
+        tables = []
+        tableContent = {}
+        #           centerTopGroup = re.search("\;\n([\S\s]+)\-\-", case)
+        #           tableContent[parser_log.CENTER_TOP] = centerTopGroup.groups()[0]
+        tableGroup = re.search("\-\-\n([\s\S]+)\.\/", case)
+        if tableGroup is not None:
+            tableGroupContent_temp = tableGroup.groups()[0].strip()
+            tableGroupContent = re.sub('[\S\ ]+completed[\S\ \n]{0,}', '', tableGroupContent_temp)
+            table = parser_log.parseTable(tableGroupContent, "[\|\ ]{1,}")
+            tableContent[parser_log.I_TABLE] = table
+        tables.append(tableContent)
+        caseDict[parser_log.TABLES] = tables
+        result.append(caseDict)
+    outfp.write(json.dumps(result))
+    return result
 if __name__ == "__main__":
-    infp = open("weighttp_client_1_output.log", "r")
-    content = infp.read()
-    outfp = open("2.txt", "a+")
-    for data in content:
-        a = nginx_parser(data, outfp)
-        #print a
-    
+    infile = "nginx_output.log"
+    outfile = "nginx_json.txt"
+    outfp = open(outfile, "a+")
+    nginx(infile, outfp)
     outfp.close()
-    infp.close()
